4c9616b66e31cb46d62f121cf2410679f73cf54b
Maximilian Schäfer <info.m.schaefer@gmail.com>
2021-01-25 16:40:51 +0100

    added more changes

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("This is a test!");
-		System.out.println("Some change"),
+		System.out.println("More changes"),
 	}
 
 }
